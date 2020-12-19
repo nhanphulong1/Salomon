5e43186772af3504ff3c9d2a5727d220fc11ce22
STUDENT <nhanb1706619@student.ctu.edu.vn>
2020-12-19 09:07:19 +0700

    Update thu vien

---
+++ backend/layouts/script.php
@@ -0,0 +1,3 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/assets/vendor/jquery/jquery-3.5.1.min.js"></script>
+<script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
---
+++ backend/layouts/style.php
@@ -0,0 +1,7 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="assets\vendor\bootstrap\css\bootstrap.css.map" type="text/css" />
+<!-- Font awesome -->
+<link rel="stylesheet" href="assets\vendor\font-awesome\css" type="text/css" />
+
+<!-- Custom css - Các file css do chúng ta tự viết -->
+<link rel="stylesheet" href="/assets/backend/css/style.css" type="text/css" />
