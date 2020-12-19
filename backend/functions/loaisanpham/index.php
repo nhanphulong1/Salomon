16d4c74523fe7afe0e6bedf2c91de268b572d141
STUDENT <nhanb1706619@student.ctu.edu.vn>
2020-12-19 09:50:42 +0700

    Tao trang index loaisanpham

---
+++ backend/functions/loaisanpham/index.php
@@ -0,0 +1,39 @@
+<!-- Nhúng file cấu hình để xác định được Tên và Tiêu đề của trang hiện tại người dùng đang truy cập -->
+<?php include_once(__DIR__ . '/../../layouts/config.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Nhúng phần file head.php -->
+    <?php include_once(__DIR__.'/../../layouts/head.php'); ?>
+</head>
+<body>
+    <!-- Header -->
+    <?php include_once(__DIR__ . '/../../layouts/partials/header.php'); ?>
+    <!-- end header -->
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+            <!-- sidebar -->
+                <?php include_once(__DIR__ . '/../../layouts/partials/sidebar.php'); ?>
+            <!-- end sidebar -->
+            </div>
+            <!-- Content -->
+            <div class="Content">
+                <h1>Nội dung</h1>
+            </div>
+            <!-- end Content -->
+        </div>
+    </div>
+
+    <!-- footer -->
+    <?php include_once(__DIR__ . '/../../layouts/partials/footer.php'); ?>
+    <!-- end footer -->
+
+    <!-- Nhúng file quản lý phần SCRIPT JAVASCRIPT -->
+    <?php include_once(__DIR__ . '/../../layouts/scripts.php'); ?>
+
+    <!-- Các file Javascript sử dụng riêng cho trang này, liên kết tại đây -->
+    <!-- <script src="..."></script> -->
+</body>
+</html>
